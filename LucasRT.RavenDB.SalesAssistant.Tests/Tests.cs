2f39f1175581ae7b244058bf90bcfd77ab1c1a35
Tavares, Lucas (CONTRACTOR) <lucas.tavares@bp.com>
2025-09-24 17:34:07 -0300

    Sample CSV and test case fix.

--- LucasRT.RavenDB.SalesAssistant.Tests/Tests.cs
+++ LucasRT.RavenDB.SalesAssistant.Tests/Tests.cs
@@ -5,6 +5,7 @@ using LucasRT.RavenDB.SalesAssistant.RestApi.Domain.Entities.Leads;
 using LucasRT.RavenDB.SalesAssistant.Tests.Fixtures.Databases;
 using Microsoft.AspNetCore.Http;
 using System.Reflection;
+using static LeadSoft.Common.Library.Enumerators.Enums;
 
 namespace LucasRT.RavenDB.SalesAssistant.Tests
 {
@@ -13,16 +14,31 @@ namespace LucasRT.RavenDB.SalesAssistant.Tests
         public readonly ILeadService leadService = new LeadService(fixture.RavenDB);
 
         [Fact]
-        public async Task CSVImport()
+        public async Task List1Import()
         {
-            using Stream csvStream = Assembly.GetExecutingAssembly().GetEmbeddedResourceStream($"YourLeads.csv");
-            IFormFile csvFile = new FormFile(csvStream, 0, csvStream.Length, "Data", "YourLeads.csv")
+            using Stream csvStream = Assembly.GetExecutingAssembly().GetEmbeddedResourceStream($"List1.csv");
+            IFormFile csvFile = new FormFile(csvStream, 0, csvStream.Length, "Data", "List1.csv")
             {
                 Headers = new HeaderDictionary(),
                 ContentType = "text/csv"
             };
 
-            DTOBoolResponse dto = await leadService.ImportAsync(csvFile, LeadOrigin.Other);
+            DTOBoolResponse dto = await leadService.ImportAsync(csvFile, CultureName.pt_BR, LeadOrigin.List1);
+
+            Assert.True(dto.IsTrue());
+        }
+
+        [Fact]
+        public async Task List2Import()
+        {
+            using Stream csvStream = Assembly.GetExecutingAssembly().GetEmbeddedResourceStream($"List2.csv");
+            IFormFile csvFile = new FormFile(csvStream, 0, csvStream.Length, "Data", "List2.csv")
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = "text/csv"
+            };
+
+            DTOBoolResponse dto = await leadService.ImportAsync(csvFile, CultureName.pt_BR, LeadOrigin.List2);
 
             Assert.True(dto.IsTrue());
         }
